5951aea5faff39e891d400bb77faac512303485e
Sarthak <srtkverma@gmail.com>
2019-06-04 19:00:28 +0530

    add buttons and basic functions

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("it works");
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operate(a,b,c){
+    switch(b){
+        case "+":
+            return add(a,c);
+        case "-":
+            return sub(a,c);
+        case "*":
+            return multiply(a,c);
+        case "/":
+            return divide(a,c);
+    }
+}
